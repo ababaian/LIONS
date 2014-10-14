1d6db1e5fe8b60aaf23b90f4c7f4a62b7aa1db41
artem <artem@glitch>
2014-10-14 14:06:59 -0700

    test update to lion.sh

--- lions.sh
+++ lions.sh
@@ -1,8 +1,17 @@
 #!/bin/bash/
-# 
-# lions.sh
-# ===================================================================
+# Usage:  lions.sh <input.list> <paramter.ctrl>
 #
+# ===================================================================
 # LIONS analysis pipeline
-#
 # ===================================================================
+#
+# Analyze an input .bam RNAseq file for transcripts which initiate in
+# transposable elements and create an annotation file. Compare TE
+# files between biological groups.
+#
+# Details can be found in LIONS.txt
+#
+
+
+# INITIALIZATION ===================================================
+#
